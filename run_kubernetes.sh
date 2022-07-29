0d50c49393c06e44ee01521bab7d2ee64ef3e510
EC2 Default User <ec2-user@ip-172-31-85-204.ec2.internal>
2022-07-29 17:22:44 +0000

    setup complete

---
+++ run_kubernetes.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# This tags and uploads an image to Docker Hub
+
+# Step 1:
+# This is your Docker ID/path
+dockerpath=phoenix5050/sklearn:v1.0
+
+# Step 2
+# Run the Docker Hub container with kubernetes
+kubectl run sklearn\
+    --image=$dockerpath\
+    --port=80 --labels app=sklearn 
+
+
+# Step 3:
+# List kubernetes pods
+kubectl get pods
+
+# Step 4:
+# Forward the container port to a host
+kubectl port-forward sklearn 8000:80
+
